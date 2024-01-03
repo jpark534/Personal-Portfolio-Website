ec981ede66436cef7f0409dde6e628bd8aee3d8b
Julia <jparkhyun334@gmail.com>
2024-01-03 18:15:49 -0500

    updated project with basic files

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return(  
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title"> Hey, I'm Julia</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            LALALALLALALALALALLALA.
+            <br /> blah blah blah  Lorem ipsum dolor sit.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" /> {/*this is the big man picture */}
+      </div>
+    </section>
+  );
+}
