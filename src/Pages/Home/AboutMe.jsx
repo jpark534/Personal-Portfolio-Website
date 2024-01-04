c8a78163016b1081897a6bbbe1501eb8d882ce39
jpark534 <jparkhyun334@gmail.com>
2024-01-04 16:52:22 -0500

    updated about me page

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,10 @@
+export default function AboutMe() {
+  return(
+    <section id= "About Me" className="about--section">
+      <div className="about--section--img">
+        <img src= "./img/about-me.png" alt="About Me"/>
+      </div>
+
+    </section>
+  )
+}
