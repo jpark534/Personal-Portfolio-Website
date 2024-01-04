930136738932287e4bf8ce410c46514584c9b9c9
jpark534 <jparkhyun334@gmail.com>
2024-01-03 19:11:24 -0500

    removed extra line in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/Home/Navbar';
 import Home from './Pages/Home/Homescreen';
 
-
 function App() {
   return (
     <div className="App">
